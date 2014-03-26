b5603fcc90577e1be2659bde8ba25f0d4c167a8c
Nicholas Valbusa <info@squallstar.it>
2014-03-26 12:01:26 +0000

    first commit

---
+++ Data/Account.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectorRT.Data
+{
+    public class Account
+    {
+        Windows.Storage.ApplicationDataContainer settings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+        private static string AccountLogged = "Account.Logged";
+        private static string AccountEmail = "Account.Email";
+        private static string AccountPassword = "Account.Password";
+        //private static string AccountLastSync = "Account.LastSync";
+
+        public static Account Current
+        {
+            get
+            {
+                return (Windows.UI.Xaml.Application.Current as App).account;
+            }
+        }
+
+        public bool isLoggedIn
+        {
+            get
+            {
+                return settings.Values.ContainsKey(AccountLogged);
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return settings.Values.ContainsKey(AccountEmail) ? settings.Values[AccountEmail].ToString() : null;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return settings.Values.ContainsKey(AccountPassword) ? settings.Values[AccountPassword].ToString() : null;
+            }
+        }
+
+        public void Logout()
+        {
+            settings.Values.Remove(AccountLogged);
+            settings.Values.Remove(AccountEmail);
+            settings.Values.Remove(AccountPassword);
+        }
+
+        public async Task<bool> Login(string email, string password)
+        {
+            var success = await API.AccountLogin(email, password);
+                
+            if (success)
+            {
+                settings.Values[AccountLogged] = true;
+                settings.Values[AccountEmail] = email;
+                settings.Values[AccountPassword] = password;
+            }
+
+            return success;
+        }
+    }
+}
---
+++ Login.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Il modello di elemento per la pagina vuota è documentato all'indirizzo http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CollectorRT
+{
+    /// <summary>
+    /// Pagina vuota che può essere utilizzata autonomamente oppure esplorata all'interno di un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            txtEmail.IsEnabled = false;
+            txtPassword.IsEnabled = false;
+            btnLogin.IsEnabled = false;
+            loader.IsActive = true;
+
+            var success = await (Application.Current as App).account.Login(txtEmail.Text, txtPassword.Text);
+
+            if (!success)
+            {
+                txtEmail.IsEnabled = true;
+                txtPassword.IsEnabled = true;
+                txtPassword.Text = "";
+                btnLogin.IsEnabled = true;
+                loader.IsActive = false;
+
+                MessageDialog msgDialog = new MessageDialog("The email address or password you entered are wrong.", "Login details invalid");
+
+                UICommand okBtn = new UICommand("OK");
+                msgDialog.Commands.Add(okBtn);
+
+                await msgDialog.ShowAsync();
+
+                txtPassword.Focus(FocusState.Keyboard);
+            }
+            else
+            {
+                btnLogin.Visibility = Visibility.Collapsed;
+                txtEmail.Visibility = Visibility.Collapsed;
+                txtPassword.Visibility = Visibility.Collapsed;
+                loader.IsActive = true;
+            }
+        }
+    }
+}
