9916c25329c22e7d26902e70f4367a1fb5c7828c
Nicholas Valbusa <info@squallstar.it>
2014-03-27 14:42:11 +0000

    collections view

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking.Connectivity;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -27,8 +28,59 @@ namespace CollectorRT
         public MainPage()
         {
             this.InitializeComponent();
-
             txtMessage.Visibility = Visibility.Collapsed;
+
+            if (Account.Current.isLoggedIn)
+            {
+                ConnectionProfile profile = NetworkInformation.GetInternetConnectionProfile();
+
+                if (profile.GetNetworkConnectivityLevel() >= NetworkConnectivityLevel.InternetAccess)
+                {
+                    LoadCollections();
+                }
+                else
+                {
+                    Proceed();
+                }
+            }
+        }
+
+        private void Proceed()
+        {
+            this.Frame.Navigate(typeof(CollectionsView));
+        }
+
+        private async void LoadCollections()
+        {
+            txtDescription.Visibility = Visibility.Collapsed;
+            btnLogin.Visibility = Visibility.Collapsed;
+            txtEmail.Visibility = Visibility.Collapsed;
+            txtPassword.Visibility = Visibility.Collapsed;
+            loader.IsActive = true;
+            txtMessage.Visibility = Visibility.Visible;
+
+            var syncSuccess = await Account.Current.Sync();
+            if (syncSuccess)
+            {
+                GetCollectionArticles();
+            }
+            else
+            {
+                MessageDialog msgDialog = new MessageDialog("We couldn't sync your account details. Please try to login with your Collector account on your Windows Phone, then try again to log here with the same account.", "Sync failed");
+
+                UICommand okBtn = new UICommand("OK");
+                msgDialog.Commands.Add(okBtn);
+
+                await msgDialog.ShowAsync();
+
+                txtDescription.Visibility = Visibility.Visible;
+                txtEmail.IsEnabled = true;
+                txtPassword.IsEnabled = true;
+                txtPassword.Text = "";
+                btnLogin.IsEnabled = true;
+                loader.IsActive = false;
+                txtMessage.Visibility = Visibility.Collapsed;
+            }
         }
 
         private async void btnLogin_Click(object sender, RoutedEventArgs e)
@@ -59,35 +111,7 @@ namespace CollectorRT
             }
             else
             {
-                txtDescription.Visibility = Visibility.Collapsed;
-                btnLogin.Visibility = Visibility.Collapsed;
-                txtEmail.Visibility = Visibility.Collapsed;
-                txtPassword.Visibility = Visibility.Collapsed;
-                loader.IsActive = true;
-                txtMessage.Visibility = Visibility.Visible;
-
-                var syncSuccess = await Account.Current.Sync();
-                if (syncSuccess)
-                {
-                    GetCollectionArticles();
-                }
-                else
-                {
-                    MessageDialog msgDialog = new MessageDialog("We couldn't sync your account details. Please try to login with your Collector account on your Windows Phone, then try again to log here with the same account.", "Sync failed");
-
-                    UICommand okBtn = new UICommand("OK");
-                    msgDialog.Commands.Add(okBtn);
-
-                    await msgDialog.ShowAsync();
-
-                    txtDescription.Visibility = Visibility.Visible;
-                    txtEmail.IsEnabled = true;
-                    txtPassword.IsEnabled = true;
-                    txtPassword.Text = "";
-                    btnLogin.IsEnabled = true;
-                    loader.IsActive = false;
-                    txtMessage.Visibility = Visibility.Collapsed;
-                }
+                LoadCollections();
             }
         }
 
@@ -99,12 +123,13 @@ namespace CollectorRT
 
             foreach (var collection in collections)
             {
-                txtMessage.Text = String.Format("Getting collections data...\r\n{0} collections left", toUpdate);
+                txtMessage.Text = String.Format("Fetching articles...\r\n{0} collections left", toUpdate);
                 await collection.update();
                 toUpdate--;
             }
 
-            txtMessage.Text = "Finishing up...";
+            txtMessage.Text = "Clearing up...";
+            Proceed();
         }
     }
 }
