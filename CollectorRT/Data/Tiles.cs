09e74fbb1ea9f244514e2d8eb6be2c16dd578f0d
Nicholas Valbusa <info@squallstar.it>
2014-04-04 15:41:23 +0100

    random colors

--- CollectorRT/Data/Tiles.cs
+++ CollectorRT/Data/Tiles.cs
@@ -10,6 +10,9 @@ namespace CollectorRT.Data
 {
     class Tiles
     {
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+
         /// <summary>
         /// Returns the path of the image for a given category
         /// </summary>
@@ -160,7 +163,38 @@ namespace CollectorRT.Data
                 return Color.FromArgb(255, 52, 80, 107);
             }
 
-            return Color.FromArgb(255, 188, 45, 48);
+            return GetRandomColor();
+        }
+
+        private static Color GetRandomColor()
+        {
+            Color[] c = new Color[16];
+            c[0] = Color.FromArgb(255, 188, 45, 48);
+            c[1] = Color.FromArgb(255, 0, 193, 197);
+            c[2] = Color.FromArgb(255, 0, 151, 197);
+            c[3] = Color.FromArgb(255, 197, 74, 0);
+            c[4] = Color.FromArgb(255, 181, 197, 0);
+            c[5] = Color.FromArgb(255, 0, 90, 197);
+            c[6] = Color.FromArgb(255, 93, 0, 197);
+            c[7] = Color.FromArgb(255, 197, 0, 155);
+            c[8] = Color.FromArgb(255, 197, 0, 78);
+            c[9] = Color.FromArgb(255, 254, 87, 90);
+            c[10] = Color.FromArgb(255, 105, 206, 174);
+            c[11] = Color.FromArgb(255, 83, 109, 185);
+            c[12] = Color.FromArgb(255, 193, 213, 114);
+            c[13] = Color.FromArgb(255, 96, 174, 188);
+            c[14] = Color.FromArgb(255, 102, 96, 188);
+            c[15] = Color.FromArgb(255, 241, 93, 74);
+
+            return c[RandomNumber(0, c.Length)];
+        }
+
+        public static int RandomNumber(int min, int max)
+        {
+            lock (syncLock)
+            { // synchronize
+                return random.Next(min, max);
+            }
         }
     }
 }
